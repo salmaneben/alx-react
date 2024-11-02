b988c610e98eb2541c2acdbf4c445a001260c56b
salmaneben <salmanebenyakhlaf95@gmail.com>
2024-11-02 14:53:08 +0100

    update

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,12 @@
+import $ from 'jquery';
+
+$(document).ready(function() {
+  // Add first paragraph
+  $('body').append('<p>Holberton Dashboard</p>');
+  
+  // Add second paragraph
+  $('body').append('<p>Dashboard data for the students</p>');
+  
+  // Add third paragraph
+  $('body').append('<p>Copyright - Holberton School</p>');
+});
