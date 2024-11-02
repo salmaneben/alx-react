240a7f85972cd66017ac6026077b6ef972795714
salmaneben <salmanebenyakhlaf95@gmail.com>
2024-11-02 03:15:00 +0100

    update

---
+++ 0x01. React intro/task_0/dashboard/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import holbertonLogo from './assets/holberton-logo.jpg';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={holbertonLogo} alt="Holberton Logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <div className="App-footer">
+        <p>Copyright 2020 - Holberton School</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
